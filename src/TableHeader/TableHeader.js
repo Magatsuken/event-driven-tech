c8dfd7d67be609684cbf3e9a9f73d49b24ca7690
Nicholas Truong <magatsuken@gmail.com>
2023-06-18 14:08:09 -0400

    Create TableHeader component

---
+++ src/TableHeader/TableHeader.js
@@ -0,0 +1,16 @@
+
+
+export default function TableHeader() {
+    return (
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Gender</th>
+                <th>IP Address</th>
+            </tr>
+        </thead>
+    )
+}
